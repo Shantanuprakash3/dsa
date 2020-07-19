f2705f3c468bf045e9d3a59b1734292297b7b26a
Shantanu Prakash <shantanuprakash@Shantanus-MacBook-Air.local>
2020-07-20 00:29:35 +0530

    long time no c -_- back to basics, isValidBST()

---
+++ bst.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct treeNode{
+	int val;
+	treeNode* left;
+	treeNode* right;
+}
+
+bool isValidtBSTUtil(treeNode* A, int min, int max){
+	if(A==NULL){
+		return true;
+	}
+	if(A->val < min || A->val > max){
+		return false;
+	}
+	return isValidBSTUtil(A->left, min, A->val-1) && isValidBSTUtil(A->right, max, A->val);
+}
+
+bool isValidBST(treeNode* A){
+	if(A==NULL){
+		return true;
+	}
+	return isValidBSTUtil(A, INT_MIN, INT_MAX);
+}
+
+int main(){
+	# populate tree
+	# call isValidBST
+	return 0;
+}
