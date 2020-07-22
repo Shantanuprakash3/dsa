3d8ca829338ca1e46c42d3435195625867897240
Shantanu Prakash <shantanuprakash@Shantanus-MacBook-Air.local>
2020-07-22 15:52:25 +0530

    adding level order traversal

--- tree_traversal.cpp
+++ tree_traversal.cpp
@@ -26,6 +26,38 @@ void preOrder (treeNode *root){
 	preOrder(root->right);
 }
 
+void levelOrder(treeNode *root){
+	if(root==NULL){
+		return;
+	}
+	int height = treeHeight(treeNode* root);
+	
+	for(int i=1;i<=height;i++){
+		printLevel(root, i);
+	}
+}
+
+void printLevel(treeNode *root, int level){
+	if(root==NULL){
+		return;
+	}
+	if (level == 1){
+		cout << root->data << " ";
+	} else if(level > 1){
+		printLevel(root->left, level-1);
+		printLevel(root->right, level-1);
+	}	
+}
+
+int treeHeight(treeNode *root){
+	if(root=NULL){
+		return 0;
+	}
+	int lheight = treeHeight(root->left);
+	int rheight = treeHeight(root->right);
+
+	return 1 + max(lheight, rheight);
+}
 int main(){
 	# populate tree
 	# call traversal methods
