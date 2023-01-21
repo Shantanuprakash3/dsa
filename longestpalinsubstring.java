5292874604607cb0add5982a72c596b0937436af
Shantanu Prakash <shantanuprakash@Shantanus-MacBook-Air.local>
2023-01-21 16:04:06 +0530

    longestpalinsubstr

---
+++ longestpalinsubstring.java
@@ -0,0 +1,39 @@
+public class LongestPalinSubstring{
+	static void printSubstr(String str, int low, int high){
+		System.out.println(str.substring(low,high+1));
+	}
+
+	static int longestPalinSubstr(String str){
+		int n = str.length();
+
+		boolean table[][] = newww boolean[n][n];
+		int maxLength = 1;
+
+		for(int i=0;i<n;i++)
+			table[i][i] = true;
+
+		int start = 0;
+
+		for(int i=0;i<n-1;i++){
+			if(s.charAt(i)==s.charAt(i+1)){
+				table[i][i+1] = 1;
+				start = i;
+				maxLength = 2;
+			}
+		}
+
+		for(int k=3;k<=n;k++){
+			for(int i=0;i<n-k+1;i++){
+				int j=i+k-1;
+				if(table[i+1][j-1]==true && s.charAt(i)==s.charAt(j)){
+					table[i][j]=true;
+					if(k > maxLength){
+						start = i;
+						maxLength = k;
+					}
+				}
+			}
+		}
+		return s.substring(start,start+maxLength);
+	}
+}
